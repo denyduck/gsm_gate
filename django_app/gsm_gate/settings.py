4953fcc39d9f1d7d4c9d75e6929ca8a58eb13401
denyduck <129299133+denyduck@users.noreply.github.com>
2025-12-15 16:33:56 +0100

    Configure static file settings in settings.py
    pridani static adresaru pro Unfold

--- django_app/gsm_gate/settings.py
+++ django_app/gsm_gate/settings.py
@@ -122,7 +122,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'static/'.
+
+# STATIC FILES - pro Unfold
+STATIC_URL = '/static/'
+STATIC_ROOT = '/usr/src/app/staticfiles/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
